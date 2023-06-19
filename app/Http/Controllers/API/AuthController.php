92f1b6b14d96458b25545715897a18dc6eadc8ad
Ishakkangah <ishakkangah@gmail.com>
2023-06-19 15:27:57 +0700

    membuat api login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -37,6 +38,39 @@ class AuthController extends Controller
                 'username'      => $user->name,
                 'token'         => $token,
                 'message'        => 'Register Successfully'
+            ]);
+        }
+    }
+
+    // Login
+    public function login(Request $request)
+    {
+        $validator              = Validator::make($request->all(), [
+            'email'             => 'required|string|email',
+            'password'          => 'required|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'validation_errors' => $validator->messages()
+            ]);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status'            => 401,
+                'message'           => 'Invalid creadentials'
+            ]);
+        } else {
+            $token                  = $user->createToken($user->email . '_Token')->plainTextToken;
+
+            return response()->json([
+                'status'            => 200,
+                'username'          => $user->name,
+                'token'             => $token,
+                'message'           => 'logged successfully.',
             ]);
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [AuthController::class, 'register']);
+Route::post('login',                    [AuthController::class, 'login']);
+Route::post('register',                 [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
